3fe23130460a2ccca8b311a3337b9a52474f4860
Donya_Jafari <donyajfri111@gmail.com>
2023-05-14 01:57:13 +0330

    dijkstra

---
+++ src/Graph.java
@@ -0,0 +1,107 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class Graph
+{
+    static int nodeCount;
+    static Map<Node, List<Edge>> graph = new HashMap<>();
+
+    public static void readGraph()/////////////////////////////////////
+    {
+        int counter = 0;
+        try (Scanner scanner = new Scanner(new File("graph.txt")))
+        {
+            String[] firstRow = scanner.nextLine().split(" ");
+            nodeCount = Integer.parseInt(firstRow[0]);
+            for(int i=1; i<nodeCount+1; i++)
+            {
+                Node created = new Node(i);
+                Node.nodes.add(created);
+                List<Edge> relatedEdges = new ArrayList<>();
+                graph.put(created,relatedEdges);
+            }
+            while (scanner.hasNextLine())
+            {
+                String[] tokens = scanner.nextLine().split(" ");
+                int startID = Integer.parseInt(tokens[0]);
+                int endID = Integer.parseInt(tokens[1]);
+                int weight = Integer.parseInt(tokens[2]);
+                counter++;
+                Edge NEW = new Edge(counter,Node.getNodeByID(startID),Node.getNodeByID(endID),weight);
+                Edge.edges.add(NEW);
+                List<Edge> to_be_updated = graph.get(Node.getNodeByID(startID));
+                to_be_updated.add(NEW);
+                to_be_updated = graph.get(Node.getNodeByID(endID));
+                to_be_updated.add(NEW);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Node> findPath(Map<Node, List<Edge>> graph, Node start,Node goal) ////////////////////////
+    {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        Map<Node, Integer> gScores = new HashMap<>();
+        for(int i=1; i<nodeCount+1; i++)
+            gScores.put(Node.getNodeByID(i),0);
+        Map<Node, Node> parents = new HashMap<>();
+
+        start.score=0;
+        queue.add(start);
+
+        while (!queue.isEmpty())
+        {
+            Node current = queue.poll();
+            if (current==goal)
+            {
+                List<Node> path = new ArrayList<>();
+                while (parents.containsKey(current))
+                {
+                    path.add(current);
+                    current = parents.get(current);
+                }
+                path.add(start);
+                Collections.reverse(path);
+                return path;
+            }
+            for (Edge neighbor : graph.get(current))
+            {
+                Integer tentativeGScore = gScores.getOrDefault(current, Integer.MAX_VALUE) + neighbor.weight;
+                Node end = new Node();
+                if(neighbor.node1==current)
+                    end = neighbor.node2;
+                else if(neighbor.node2==current)
+                    end = neighbor.node1;
+                if (tentativeGScore < end.score)
+                {
+                    end.score=tentativeGScore;
+                    gScores.put(end, tentativeGScore);
+                    parents.put(end, current);
+                    queue.add(end);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void showPath (int ID1, int ID2)////////////////////
+    {
+        Node.resetScores();
+        readGraph();
+        Node start = Node.getNodeByID(ID1);
+        Node end = Node.getNodeByID(ID2);
+        List<Node> path = findPath(graph,start,end);
+        for(int i=0; i< path.size(); i++)
+            System.out.println(path.get(i).ID);
+    }
+
+}
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,36 @@
-public class Node
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node implements Comparable<Node>
 {
+    static ArrayList<Node> nodes = new ArrayList<>();
     int ID;
+    int score = Integer.MAX_VALUE;
+    ArrayList<Edge> edges = new ArrayList<>();
+    Node(int ID)
+    {
+        this.ID=ID;
+    }
+    Node(){}
+    public static Node getNodeByID(int ID)
+    {
+        for(int i=0; i<nodes.size(); i++)
+            if(nodes.get(i).ID==ID)
+                return nodes.get(i);
+        return null;
+    }
+    public static void resetScores()
+    {
+        for(int i=0; i<nodes.size(); i++)
+            nodes.get(i).score=Integer.MAX_VALUE;
+    }
+    public int compareTo(Node other)
+    {
+        if(other.score>this.score)
+            return -1;
+        else if(other.score==this.score)
+            return 0;
+        else
+            return 1;
+    }
 }
