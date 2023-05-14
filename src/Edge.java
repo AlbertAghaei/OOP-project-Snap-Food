6e09a930e05efa970cd0284472526eb0ad6f281d
albert <zahraaghaeepour@gmail.com>
2023-05-14 14:53:18 -0800

    changed stuff

--- src/Edge.java
+++ src/Edge.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Edge
 {
     static ArrayList<Edge> edges = new ArrayList<>();
-    Node node1;
+    Node node1;// salam hoora
     Node node2;
     int weight;
     int ID;
