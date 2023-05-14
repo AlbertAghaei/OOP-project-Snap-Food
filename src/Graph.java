b58d61cd8570c3836be70b8bb64b8fdbe1b4f17b
Donya_Jafari <donyajfri111@gmail.com>
2023-05-14 22:09:12 +0330

    dijkstra2

--- src/Graph.java
+++ src/Graph.java
@@ -10,7 +10,6 @@ import java.io.FileNotFoundException;
 public class Graph {
     static int nodeCount;
     static Map<Node, List<Edge>> graph = new HashMap<>();
-
     public static void readGraph()/////////////////////////////////////
     {
         int counter = 0;
