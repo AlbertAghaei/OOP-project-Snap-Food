00f5044e34388c8525a4c2b481a25cf6f06b6992
albert <zahraaghaeepour@gmail.com>
2023-05-19 16:04:47 -0800

    debuging regexes

--- src/InputManager.java
+++ src/InputManager.java
@@ -5,25 +5,25 @@ public class InputManager
  Scanner scanner=new Scanner(System.in);
  String rawinput=scanner.nextLine();
  String[] input=rawinput.split(" ");
- String addAdmin="ADD ADMIN [a-zA-Z]+ \\w{8,}\\s*";
- String addUser="ADD USER [a-zA-Z]+ \\w{8,}\\s*";
- String loginAdmin="LOGIN ADMIN [a-zA-Z]+ \\w{8,}\\s*";
- String loginUser="LOGIN USER [a-zA-Z]+ \\w{8,}\\s*";
- String logout="LOGOUT";
+ String addAdmin="ADD ADMIN [a-zA-Z]+ \\w\\s*";
+ String addUser="ADD USER [a-zA-Z]+ \\w\\s*";
+ String loginAdmin="LOGIN ADMIN [a-zA-Z]+ \\w\\s*";
+ String loginUser="LOGIN USER [a-zA-Z]+ \\w\\s*";
+ String logout="LOGOUT\\s*";
  //resturant owner orders regex//
- String selectResturan="SELECT \\d\\s*";
+ String selectResturan="SELECT \\d+\\s*";
  String showLocation="SHOW LOCATION\\s*";
- String editLocation="EDIT LOCATION \\d\\s*";
+ String editLocation="EDIT LOCATION \\d+\\s*";
  String showFoodtype="SHOW FOODTYPE";
 // String editFoodtype
  String selectMenu="SELECT MENU";
 // String editFood
  String addFood="ADD FOOD [a-zA-Z]+ -?\\d*(\\.\\d+)?\\s*";
- String deleteFood="DELETE FOOD \\d\\s*";
- String deactiveFood="DEACTIVE FOOD \\d\\s*";
- String activeFood="ACTIVE FOOD \\d\\s*";
- String discountFood="DISCOUNT FOOD \\d \\d \\d\\s*";
- String selectFood="SELECT FOOD \\d\\s*";
+ String deleteFood="DELETE FOOD \\d+\\s*";
+ String deactiveFood="DEACTIVE FOOD \\d+\\s*";
+ String activeFood="ACTIVE FOOD \\d+\\s*";
+ String discountFood="DISCOUNT FOOD \\d+ \\d+ \\d+\\s*";
+ String selectFood="SELECT FOOD \\d+\\s*";
  String displayRating="DISPLAY RATING";
  String displayComments="DISPLAY COMMENTS";
 // String addNewResponse
@@ -33,15 +33,15 @@ public class InputManager
  String showOrderHistory="SHOW ORDER HISTORY";
  //customer orders regex//
  String searchResturant="SEARCH RESTURANT [a-zA-Z]+ \\s*";
- String searchFood="SEARCH \\d\\s*";
- String selectFoodCustomer="SELECT \\d\\s*";
+ String searchFood="SEARCH \\d+\\s*";
+ String selectFoodCustomer="SELECT \\d+\\s*";
  // String addNewComment
 // String editComment
- String submitRating="SUBMIT RATING \\d\\s*";//rating must be a digit from 0to5
- String editRating="EDUT RATING \\d\\s*";
+ String submitRating="SUBMIT RATING \\d+\\s*";//rating must be a digit from 0to5
+ String editRating="EDUT RATING \\d+\\s*";
  String addFoodToCart="ADD THIS FOOD TO CARTR";
  String accessOrserHistory="ACCESS ORDER HISTORY";
- String selectOrder="SELECT ORDER \\d\\s*";
+ String selectOrder="SELECT ORDER \\d+\\s*";
  String displayCartStatus="DISPLAY CART STATUS";
  String confirmOrder="CONFIRM ORDER";
  String showEstimatedDeliveryTime="SHOW ESTIMATED DELIVERY TIME";
@@ -52,7 +52,7 @@ public class InputManager
  //deliverer regex//
  String undeliveredOrders="SHOW UNDELIVERED ORDERS";
  //path regexes//
- String findBestPath="FIND BEST PATH \\d\\s*";
+ String findBestPath="FIND BEST PATH \\d+\\s*";
  String showPath="SHOW PATH";
  String showDeliveryTime="SHOW DELIVERY TIME";
 
