289833a2f70207823dc8c1fc3e38f77e601879c8
Donya_Jafari <donyajfri111@gmail.com>
2023-05-24 23:07:55 +0330

    normal funcs 4

--- src/InputManager.java
+++ src/InputManager.java
@@ -4,7 +4,7 @@ public class InputManager
    public static void funcCaller(String input) throws SQLException
    {
        String[] splitted = input.split(" ");
-       if(splitted.length==4 && (splitted[0]+" "+splitted[1]).equals("ADD ADMIN"))///////////////
+       if(splitted.length==4 && (splitted[0]+" "+splitted[1]).equals("ADD ADMIN"))//////////////
          Entrance.registerOwner(splitted[2],splitted[3]);
        else if(splitted.length==4 && (splitted[0]+" "+splitted[1]).equals("ADD USER"))///////////////
            Entrance.registerNormal(splitted[2],splitted[3]);
